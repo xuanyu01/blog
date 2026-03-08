40de8042c7eba1f2f2a4c6612271e9c433c93926
xuanyu <502874908@qq.com>
2026-03-08 15:44:39 +0800

    加入注册和登录系统,暂未有session验证

--- route.go
+++ route.go
@@ -9,6 +9,7 @@ import (
 )
 
 func RegisterRouters(r *gin.Engine, db *sql.DB) {
+	//主页
 	r.GET("/", func(c *gin.Context) {
 		blogs, err := getBlogs(db)
 		if err != nil {
@@ -20,16 +21,62 @@ func RegisterRouters(r *gin.Engine, db *sql.DB) {
 		})
 	})
 
+	//博客提交模块
 	r.GET("/submit", func(c *gin.Context) {
 		c.JSON(400, gin.H{
 			"400": "Access method denied",
 		})
 	})
-
 	r.POST("/submit", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "POST test",
 		})
+	})
+
+	//注册模块
+	r.GET("/register", func(c *gin.Context) {
+		c.HTML(200, "register.html", gin.H{
+			"title": "Register",
+		})
+	})
+	r.POST("/register", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		success := registerNewUser(db, username, password)
+
+		if success == "Register success" {
+			c.JSON(200, gin.H{
+				"message": "Register success",
+			})
+		} else {
+			c.JSON(400, gin.H{
+				"message": success,
+			})
+		}
+	})
+
+	//登录模块
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(200, "login.html", gin.H{
+			"title": "Login",
+		})
+	})
+	r.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		success := LoginUser(db, username, password)
+
+		if success == "Success Login" {
+			c.JSON(200, gin.H{
+				"message": "Login success",
+			})
+		} else {
+			c.JSON(400, gin.H{
+				"message": success,
+			})
+		}
 	})
 
 	if err := r.Run(":5345"); err != nil {
@@ -42,5 +89,9 @@ func LoadTemplates() *template.Template {
 		"blog_title":   func(b Blog) string { return b.Title },
 		"blog_content": func(b Blog) string { return b.Content },
 	}
-	return template.Must(template.New("templates").Funcs(funcMap).ParseFiles("templates/index.html"))
+	return template.Must(template.New("templates").Funcs(funcMap).ParseFiles(
+		"templates/index.html",
+		"templates/register.html",
+		"templates/login.html",
+	))
 }
